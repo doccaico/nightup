c145371c41c55b821be233aef7115f0ded0b8610
doccaico <hash_ext_luna@yahoo.co.jp>
2025-07-30 07:44:05 +0900

    up

--- src/go/windows.c
+++ src/go/windows.c
@@ -1,31 +1,31 @@
-#include <stdio.h> // fprintf, popen, pclose
+#include <stdio.h> // fprintf, _popen, _pclose
 #include <stdlib.h> // exit, system
-#include <unistd.h> // chdir
-#include <sys/stat.h> // stat
+#include <direct.h> // _chdir
+#include <sys/stat.h> // _stat
 
 #include "m-string.h"
 
 void go_install(const char* install_path)
 {
     // 作業ディレクトリを変更する
-    const char* working_dir = "/home/doccaico/Downloads";
-    if (chdir(working_dir) == -1) {
-        fprintf(stderr, "Error: failed to chdir '%s'\n", working_dir);
+    const char* working_dir = "C:\\Users\\doccaico\\Downloads";
+    if (_chdir(working_dir) != 0) {
+        fprintf(stderr, "Error: failed to _chdir '%s'\n", working_dir);
         exit(1);
     }
 
-    // go.jsonを取得する
+    // index.jsonを取得する
     system("curl -sSL https://go.dev/dl/?mode=json -o go.json");
-    fprintf(stdout, "Download (go.json) is done\n");
+    fprintf(stdout, "Download (index.json) is done\n");
 
     // ダウンロードURLを取得する
     FILE* stream;
     const int max_buf_size = 128;
     char buf[max_buf_size];
-    stream = popen("cat go.json | jq -r '.[0].files[] | select(.os == \"linux\" and .arch == \"amd64\" and .kind == \"archive\").filename'", "r");
+    stream = popen("cat go.json | jq -r \".[0].files[] | select(.os == \"\"windows\"\" and .arch == \"\"amd64\"\" and .kind == \"\"archive\"\").filename\"", "r");
     if (stream) {
         fgets(buf, max_buf_size, stream);
-        pclose(stream);
+        _pclose(stream);
     }
     string_t filename;
     string_init_set_str(filename, buf);
@@ -35,14 +35,13 @@ void go_install(const char* install_path)
     string_printf(url, "https://go.dev/dl/%s", string_get_cstr(filename));
     fprintf(stdout, "Download URL => '%s'\n", string_get_cstr(filename));
 
-    // TODO
-    // ダウンロード (tar.gz)
+    // ダウンロード (zip)
     string_t cmd;
     string_init(cmd);
     string_printf(cmd, "curl -sSOL %s", string_get_cstr(url));
     system(string_get_cstr(cmd));
     string_reset(cmd);
-    fprintf(stdout, "Download (tar.gz) is done\n");
+    fprintf(stdout, "Download (zip) is done\n");
 
     // 解凍
     const char* tarname = string_get_cstr(filename);
@@ -52,24 +51,24 @@ void go_install(const char* install_path)
     fprintf(stdout, "Extraction is done\n");
 
     // インストール先のディレクトリが存在したら削除する
-    struct stat st;
-    if (stat(install_path, &st) == 0) {
-        string_printf(cmd, "rm -rf %s", install_path);
+    struct _stat st;
+    if (_stat(install_path, &st) == 0) {
+        string_printf(cmd, "rmdir /s /q %s", install_path);
         system(string_get_cstr(cmd));
         string_reset(cmd);
         fprintf(stdout, "Removed: %s\n", install_path);
     }
 
     // 移動
-    string_printf(cmd, "mv -f go %s", install_path);
+    string_printf(cmd, "move go %s > nul", install_path);
     system(string_get_cstr(cmd));
     string_reset(cmd);
     fprintf(stdout, "Moved: %s\n", install_path);
 
-    // zipとgo.jsonを削除する
-    string_printf(cmd, "rm %s", tarname);
+    // zipとindex.jsonを削除する
+    string_printf(cmd, "del %s", tarname);
     system(string_get_cstr(cmd));
-    system("rm go.json");
+    system("del go.json");
     fprintf(stdout, "Unnecessary files deleted\n");
 
     // 文字列をfreeする
